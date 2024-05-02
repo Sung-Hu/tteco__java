1e6b35d85fa138e25a86f3849fb04fc9c69a4b39
k_dy <ehddbs1105@gmail.com>
2024-05-02 17:14:31 +0900

    0502

--- src/structure/TencoIntArray.java
+++ src/structure/TencoIntArray.java
@@ -1,19 +1,15 @@
 package structure;
 
-import java.util.Iterator;
-
 /**
- * 배열을 활용 클래스를 설계 
- * 물론 이미 자바 표준 API 개발자들이 잘 만들어 준 클래스 들이 존재 한다. 
- * 하지만 직접 기능을 확장해서
- * 만들어 보자.
+ * 배열을 활용 클래스를 설계 물론 --> 이미 자바 표준 API 개발자들이 잘 만들어 준 클래스 들이 존재 한다. 하지만 직접 기능을
+ * 확장해서 만들어 보자.
  */
 public class TencoIntArray {
 
 	int[] intArr;
-	int count; // 배열 안에 들어간 요소의 갯수를 관리
+	int count; // 배열안에 들어간 요소의 갯수
 	public final int ARRAY_SIZE;
-	public static final int ERROR_NUM = -999999999;
+	public static final int ERROR_NUM = -99999999;
 
 	public TencoIntArray() {
 		count = 0;
@@ -28,8 +24,8 @@ public class TencoIntArray {
 	}
 
 	// 기능 설계
-	// 배열 요수 제일 뒤에 값을 추가하는 기능을 가진다.
-	public void addElenent(int inputData) {
+	// 배열 요소에 제일 뒤에 값을 추가하는 기능을 가진다.
+	public void addElement(int inputData) {
 		// 방어적 코드 필요
 		if (count >= ARRAY_SIZE) {
 			System.out.println("메모리 공간이 가득 찼습니다.");
@@ -39,57 +35,15 @@ public class TencoIntArray {
 		count++;
 	}
 
-	// 배열에 요수를 추가하는 기능
-	public void insertElement(int postion, int intputData) {
-		// 방어적 코드 1
-		if (count >= ARRAY_SIZE) {
-			System.out.println("메모리 공간이 가득 찼습니다.");
-			return;
-		}
-		// 방어적 코드 2
-		// 10 < 0
-		if (postion < 0 || ARRAY_SIZE < postion) {
-			System.out.println("지정한 인덱스 번호가 잘못 되었습니다.");
-		}
-		// 요청값 : postion -> 3
-		// [11, 12, 13, []14, 15]
-		// [11, 12, 13, [], 14, 15]
-		for (int i = count - 1; i >= postion; i--) {
-			intArr[i + 1] = intArr[i]; // 하나씩
-			// intArr[5] = 15; 수행 1
-			// intArr[4] = 14; 수행 2
-		}
-		intArr[postion] = intputData;
-		count++;
-	}
-	// 지정한 인덱스 번호에 요소를 삭제 하기
-	public void removeElement(int postion) {
-		
-		if(isEmpty()) {
-			System.out.println("삭제할 요소가 없습니다.");
-		}
-		// 인덱스 범위를 잘못 지정했다면 방어적 코드
-		if(postion < 0 || postion >= count) {
-			System.out.println("잘못된 요청 입니다.");
-		}
-		// intArr[postion]; --> 사용자가 요청한 인덱스 번호는 0 입니다.
-		// [100] [200] [300] [400]
-		// [200] [300] [400] [] 땡겨줘야 한다
-		for (int i = postion; i < count - 1; i++) {
-			intArr[i] = intArr[i + 1];
-		}
-		count--;
-	}
-
 	// 지정한 인덱스 번호에 요소를 꺼내 주기
-	public int getElement(int postion) {
-		// 배열의 크기는 10
-		// [0] [1] [2] -->
-		if (postion < 0 || postion > count - 1) {
-			System.out.println("검색 위치 오류. 현재 리스트의 갯수는 " + count + "개 입니다.");
+	public int getElement(int positon) {
+		// 배열에 크기 10
+		// [0] [1] [2] --> 3
+		if (positon < 0 || positon > count - 1) {
+			System.out.println("검색 위치 오류. 현재 리스트의 갯수는 " + count + " 개 입니다.");
 			return ERROR_NUM;
 		}
-		return intArr[postion];
+		return intArr[positon];
 	}
 
 	// 요소를 전체 출력하는 기능 만들어 주기
@@ -98,7 +52,6 @@ public class TencoIntArray {
 			System.out.println("출력할 내용이 없습니다.");
 			return;
 		}
-
 		for (int i = 0; i < intArr.length; i++) {
 			System.out.println(intArr[i]);
 		}
@@ -109,16 +62,16 @@ public class TencoIntArray {
 		for (int i = 0; i < intArr.length; i++) {
 			intArr[i] = 0;
 		}
-		// 현재 요소의 갯수 상태를 항상 관리하고 처리해야한다.
+		// 요소에 갯수 상태를 항상 관리하고 처리해야한다.
 		count = 0;
 	}
 
-	// 배열의 크기가 아닌 현재 요소의 갯수를 반환
+	// 배열에 크기가 아님 현재 요소게 갯수를 반환
 	public int getCountSize() {
 		return count;
 	}
 
-	// 현재 요소가 하나도 없는 상태이다. boolean 일땐 isEmpty 라는걸 자주쓴다
+	// 현재 요소가 하나도 없는 상태이다.
 	public boolean isEmpty() {
 		if (count == 0) {
 			return true;
@@ -127,8 +80,53 @@ public class TencoIntArray {
 		}
 	}
 
-	// 배열에 지정된 인덱스 위치에 값을 추가하는 기능
-	// 지정한 인덱스 번호에 맞는 요소를 출력하는 기능
-	// 지정한 인덱스 번호에 요소를 삭제하는 기능
+	
+	// 배열의 지정한 인덱스 위치에 값을 삽입 하기
+	public void insertElement(int positon, int inputData) {
 
+		// 방어적 코드 1
+		if (count >= ARRAY_SIZE) {
+			System.out.println("메모리 공간이 가득 찼습니다.");
+			return;
+		}
+
+		// 방어적 코드 2
+		if (positon < 0 || ARRAY_SIZE < positon) {
+			System.out.println("지정한 인덕스 번호가 잘못 되었습니다");
+			return;
+		}
+		// 요청값 : postion -> 3
+		// [11, 12, 13, 14, 15]
+		// [11, 12, 13, [], 14, 15]
+		for (int i = count - 1; i >= positon; i--) {
+			intArr[i + 1] = intArr[i]; // 하나씩
+			// intArr[5] = 15; 수행 1
+			// intArr[4] = 14; 수행 2
+		}
+		intArr[positon] = inputData;
+		count++;
+	}
+
+	// 지정한 인덱스 번호에 요소를 삭제하기 
+	public void removeElement(int positon) {
+		
+		if(isEmpty()) {
+			System.out.println("삭제 할 요소가 없습니다");
+		}
+		
+		// 인덱스 범위를 잘못 지정했다면 방어적 코드 
+		if(positon < 0 || positon >= count) {
+			System.out.println("잘못된 요청 입니다.");
+		}
+		// intArr[positon]; --> 사용자가 요청한 인덱스는 0 번이라고 가정.
+		// [100] [200] [300] [400] 
+		// [200] [300] [400] [] : 한 칸씩 앞으로 이동 
+		for (int i = positon; i < count - 1; i++) {
+			// 0 
+			// 0 + 1 
+			intArr[i] = intArr[i + 1];
+		}
+		count--;
+	}
+	
 }
